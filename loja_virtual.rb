467a3f93a14b4305d7205cc2e66ca615455a562e
mateus5473 <mateusdias200@gmail.com>
2024-11-03 14:03:34 -0300

    git commit -m "Descrição das alterações que você fez" Teste de alteração

--- loja_virtual.rb
+++ loja_virtual.rb
@@ -1,7 +1,7 @@
 class Livro
-    def initialize(autor, isbn = 1, paginas)
-        puts "Autor: #{autor}, ISBN: #{isbn}, Páginas: #{paginas}"
+    def initialize(autor, isbn = 1, paginas, ano)
+        puts "Autor: #{autor}, ISBN: #{isbn}, Páginas: #{paginas}, Ano: #{ano}"
     end
 end
 
-Livro.new "Ludwig Von Mises", 234
+Livro.new "Ludwig Von Mises", 234, 1949
